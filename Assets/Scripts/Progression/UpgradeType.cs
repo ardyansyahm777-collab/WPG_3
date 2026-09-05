22f420285c6ee52ffaee880db8c30d3663a3228a
Zzzst <ardyansyahm777@gmail.com>
2026-09-05 14:29:54 +0700

    [progression] tambah UpgradeType enum (MaxHealth/MoveSpeed/FireRate/Damage/MultiShot)

---
+++ Assets/Scripts/Progression/UpgradeType.cs
@@ -0,0 +1,15 @@
+namespace WpgGame.Progression
+{
+    /// <summary>
+    /// Tipe upgrade yang bisa dipilih pemain saat level up.
+    /// Sesuai kontrak API (Bagian 7).
+    /// </summary>
+    public enum UpgradeType
+    {
+        MaxHealth,
+        MoveSpeed,
+        FireRate,
+        Damage,
+        MultiShot
+    }
+}
